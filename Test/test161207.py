72f120f4e385a5f712e6981d7d89577cd52cc5f2
MapleInCity <987380454@qq.com>
2016-12-07 23:31:49 +0800

    2016-12-07 test

---
+++ Test/test161207.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+a = 19 + 2 * 4 - 8 / 2
+print a
+print "free" + 'a'
+print "free" + str(a)
+# 这里 repr()是一个函数，其实就是反引号的替代品，它能够把结果字符串转化为合法的 python 表达式。
+print "free" + repr(a)
+name = raw_input("What is your name?")
+age = raw_input("How old are you?")
+print "Your name is:", name
+print "You are " + age + " years old."
+after_ten = int(age) + 10
+print "You will be " + str(after_ten) + " years old after ten years."
+
+
