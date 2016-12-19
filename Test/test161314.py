ad7c646aeb3eb57cd1a2127596e6b21b84762916
MapleInCity <987380454@qq.com>
2016-12-19 09:22:10 +0800

    11

--- Test/test161314.py
+++ Test/test161314.py
@@ -1,36 +1,17 @@
 # !usr/bin/env python
 # coding:utf-8
-
 import pickle
-
 try:
-    allText = []
-    with open("E:/gitHub/learnPython/Test/test.txt") as test:
-        for each_item in test:
-            print each_item
-            allText.append(each_item)
+    with open("./test3.txt", 'wb') as mySaveData:
+        pickle.dump(["鹅鹅鹅，曲项向天歌。", "白毛浮绿水，红掌拨青波。"], mySaveData)
 
-    with open("E:/gitHub/learnPython/Test/test2.txt", 'wb') as mysavedata:
-        pickle.dump(allText, mysavedata)
-
-    with open("E:/gitHub/learnPython/Test/test2.txt", 'rb') as myloaddata:
-        mylist = pickle.load(myloaddata)
-        for each_item in mylist:
-            print each_item
-
-    with open("E:/gitHub/learnPython/Test/test3.txt", 'wb') as mysavedata2:
-        for each_item in allText:
-            mysavedata2.write(each_item)
-
-    with open("E:/gitHub/learnPython/Test/test3.txt", 'rb') as myloaddata2:
-        for each_item in myloaddata2:
-            print each_item
-
+    with open("./test3.txt", "rb") as myReadData:
+        data = pickle.load(myReadData)
+        for each_Item in data:
+            print each_Item
 except IOError as err:
-    print "File error: " + str(err)
-
+    print "IOError: " + err
 except pickle.PickleError as perr:
     print "Pickle Error: " + str(perr)
-
 finally:
     pass
