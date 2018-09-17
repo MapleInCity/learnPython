2673bcea372c7788d31675984179c9329579457f
苏龙(10014903) <sul-a@glodon.com>
2018-09-17 16:44:37 +0800

    2018-9-17 python练习

---
+++ pythonHardWay/ex10.py
@@ -0,0 +1,25 @@
+# -- coding:utf-8 --
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+littleApple = '''
+you are me little apple,
+\t\n apple
+\t\n apple
+a a a a a
+你是我的小啊小苹果，
+怎么爱你都。。。
+'''
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(littleApple)
---
+++ pythonHardWay/ex12.py
@@ -0,0 +1,9 @@
+# -- coding:utf-8 --
+
+y = input("Name? ")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+print("So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight
+))
